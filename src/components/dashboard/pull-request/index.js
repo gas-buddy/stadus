769d478556a70808ea66493c6ec2522495d8c3c8
Andre Mougeot <mougeot.andre@gmail.com>
2018-05-25 15:52:40 -0600

    Hooked up github calls

--- src/components/dashboard/pull-request/index.js
+++ src/components/dashboard/pull-request/index.js
@@ -2,16 +2,28 @@
 import React, { Component } from 'react';
 import { Segment, Header } from 'semantic-ui-react';
 import PT from 'prop-types';
+import moment from 'moment';
 import Style from './style.css';
 
+type Props = {
+  data: Object // The pull request object, will flesh this in later
+}
 
-export class PullRequest extends Component {
+export class PullRequest extends Component<Props> {
+  pullRequest: Object;
+
+  constructor(props: any) {
+    super(props);
+    this.pullRequest = props.data;
+  }
+
   render() {
-    const name = 'Implemented XYZ [BP-123] [BP-456]';
-    const branchName = 'am/implement-the-thing';
+    const name = this.pullRequest.title || '';
+    // This is gross, put this in state
+    const branchName = this.pullRequest.head.ref.replace('.json', '');
     const commentCount = Math.floor(Math.random() * 3);
 
-    const age = Math.floor(Math.random() * 14);
+    const age = moment().diff(moment(this.pullRequest.created_at), 'days');
     const decayRatio = Math.min(1.0, age / 10);
 
     const ageColor = decayRatio > 0.8 ?
@@ -26,7 +38,8 @@ export class PullRequest extends Component {
       status === 'fail' ? <span className={[Style.branchStatus, Style.fail].join(' ')}>✘</span>
       : status === 'success' ? <span className={[Style.branchStatus, Style.success].join(' ')}>✓</span>
       : <span className={[Style.branchStatus, Style.pending].join(' ')}>...</span>;
-    const jiraIssueNumbers = [ name.match(/\[([A-Z]+-[0-9]+)\]/)[1] ];
+    const jiraParse = name.match(/\[([A-Z]+-[0-9]+)\]/);
+    const jiraIssueNumbers = jiraParse && jiraParse.length? [jiraParse[1]]: [];
 
     const displayName = name.replace(/\[[A-Z]+-[0-9]+\]/g, '');
 
@@ -73,5 +86,5 @@ export class PullRequest extends Component {
 
 
 PullRequest.propTypes = {
-  id: PT.number.isRequired,
+  data: PT.object.isRequired,
 };
--- src/components/dashboard/repository/index.js
+++ src/components/dashboard/repository/index.js
@@ -2,23 +2,31 @@
 import React, { Component } from 'react';
 import { Segment, Header } from 'semantic-ui-react';
 import PT from 'prop-types';
+import * as github from '../../../utils/github';
 import Style from './style.css';
 import { PullRequest } from '../pull-request';
 
+type Props = {
+  id: number,
+  name: string,
+}
 
-export class Repository extends Component {
+export class Repository extends Component<Props> {
+  state: { id: number, collapsed: boolean, loading: boolean, name: string, latestBranch: string, prs: Object[] };
+  name: string;
+
   constructor(props: any) {
     super(props);
+
     this.toggleCollapse = this.toggleCollapse.bind(this);
     this.load = this.load.bind(this);
+    this.name = props.name;
     this.state = {
       id: props.id,
       collapsed: false,
       loading: true,
-      repo: {
-        name: 'business-pages-serv',
-        latestBranch: 'Applied latest patch #170',
-      },
+      name: props.name,
+      latestBranch: '',
       prs: [],
     };
   }
@@ -27,49 +35,49 @@ export class Repository extends Component {
     this.setState({
       loading: true,
     });
-    setTimeout(() => {
+    Promise.all([
+      github.getBranch(this.name, 'master'),
+      github.getPullRequestsForRepo(this.name),
+    ]).then(([branch, pullRequests]) => {
+      console.log('Latest branch', branch);
       this.setState({
         loading: false,
-        repo: {
-          name: 'business-pages-serv',
-          latestBranch: 'Applied latest patch #170',
-          id: Math.floor(Math.random() * 10000),
-        },
-        prs: Array.from(new Array(Math.floor(Math.random() * 7)), (a, i) => ({
-          id: i + 1,
-          age: Math.floor(Math.random() * 20),
-        })),
+        prs: pullRequests,
+        latestBranch: branch.commit.commit.message.split('\n')[0],
       });
-    }, 1300);
+    });
   }
 
   componentDidMount() {
-    console.log('loading');
     this.load();
-    // setInterval(this.load, Math.floor(Math.random() * 8000) + 5000);
+    setInterval(this.load, 60000);
   }
 
   toggleCollapse() {
     this.setState(prevState => ({
-      collapsed: !prevState.collapsed
+      collapsed: !prevState.collapsed,
     }));
   }
 
   render() {
+    let pullRequestElements = <Segment className={Style.empty}>(none)</Segment>;
+    if (this.state.prs.length) {
+      pullRequestElements = this.state.prs.map(pullRequest => (
+        <PullRequest
+          key={pullRequest.id}
+          data={pullRequest}
+        />
+      ));
+    }
     return (
       <Segment id={this.state.id} className={Style.repository} loading={this.state.loading} inverted={this.state.collapsed}>
         <Header className={Style.name} onClick={this.toggleCollapse}>
-          ▪▪▪ {this.state.repo.name}▪▪▪ 
+          ▪▪▪ {this.state.name} ▪▪▪
         </Header>
-        <Segment className={Style.status}>{this.state.repo.latestBranch}</Segment>
-        <Segment> {
-          this.state.prs.map(pullRequest => (
-            <PullRequest
-              key={pullRequest.id}
-              id={pullRequest.id}
-            />
-          ))
-        } </Segment>
+        <Segment className={Style.status}>{this.state.latestBranch}</Segment>
+        <Segment>
+          {pullRequestElements}
+        </Segment>
       </Segment>
     );
   }
@@ -77,4 +85,5 @@ export class Repository extends Component {
 
 Repository.propTypes = {
   id: PT.number.isRequired,
+  name: PT.string.isRequired,
 };
